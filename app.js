5ad0109aaadb541533f65aa3cdc99d1ec52334c5
Joshua W <106320744+J-Wil21@users.noreply.github.com>
2023-02-20 15:32:33 +0000

    Update on Js.app file

--- app.js
+++ app.js
@@ -3,22 +3,37 @@ const container = document.getElementById('con');
 const disColor = document.querySelectorAll('#displayColor');
 const label = document.querySelectorAll('#colorNum');
 
-
-btn.addEventListener('click', () => {
-
-    disColor.forEach(disColor => {
-        disColor.style.backgroundColor = randomC();
-    });
-    label.forEach(label => {
-        label.innerText = randomC();
-    });
-})
-
 function randomC() {
     let r = Math.floor(Math.random(1) * 255)
     let g = Math.floor(Math.random(1) * 255)
     let b = Math.floor(Math.random(1) * 255)
-    return `rgb(${r},${g},${b})`;
+    return `rgb(${r},${g},${b})`
 }
+
+
+btn.addEventListener('click', () => {
+    
+    for (let i = 0; i < disColor.length; i++) {
+        const C = randomC();
+        disColor[i].style.backgroundColor = C;
+        label[i].innerText = C;
+    }
+
+    // disColor.forEach(disColor => { 
+    //     const C = randomC()
+    //     disColor.style.backgroundColor = C;
+    // });
+    
+    // disColor.forEach(disColor => { 
+    //     const C = randomC()
+    //     disColor.style.backgroundColor = C;
+    // });
+
+    //     label.forEach(label => {
+    //     label.innerText = randomC();
+    //   });
+})
+
+
 
 
